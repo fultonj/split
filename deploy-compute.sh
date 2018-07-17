a8273f23f81d0f556692215dd02c823db42e7edc
John Fulton <fulton@redhat.com>
2018-07-17 15:11:26 +0000

    update deploy-compute.sh to use docker, disable telemetry and use low memory

--- deploy-compute.sh
+++ deploy-compute.sh
@@ -8,6 +8,10 @@ fi
 
 openstack overcloud deploy --templates ~/templates \
 	  -r compute_only_roles_data.yaml \
+	  -e ~/templates/environments/docker.yaml \
+	  -e ~/templates/environments/low-memory-usage.yaml \
+	  -e ~/templates/environments/disable-telemetry.yaml \
+	  -e ~/docker_registry.yaml \
 	  -e endpoint.yaml \
 	  -e extra_hosts.yaml \
 	  --stack compute0
